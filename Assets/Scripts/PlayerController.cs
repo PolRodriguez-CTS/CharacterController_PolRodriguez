8b4345eda87db9925b07787bd670fe8f5b0cd284
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-10-08 12:46:37 +0200

    cámara, movimiento y salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-using System.Numerics;
+//using System.Numerics;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +12,9 @@ public class PlayerController : MonoBehaviour
     private InputAction _moveAction;
     private Vector2 _moveInput;
     private InputAction _jumpAction;
+    private InputAction _lookAction;
+    [SerializeField] private Vector2 _lookInput;
+    
 
     [SerializeField] private float _movementSpeed = 5;
     [SerializeField] private float _jumpHeight = 2;
@@ -28,13 +32,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] float _sensorRadius;
 
-
+    private Transform _mainCamera;
 
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
         _moveAction = InputSystem.actions["Move"];
         _jumpAction = InputSystem.actions["Jump"];
+        _lookAction = InputSystem.actions["Look"];
+
+        //La cámara principal tiene que tener la tag de main camera
+        _mainCamera = Camera.main.transform;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -46,7 +54,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovimientoCutre();
+        _moveInput = _moveAction.ReadValue<Vector2>();
+        _lookInput = _lookAction.ReadValue<Vector2>();
+
+        Movement();
+        //MovimientoCutre();
+        //Movimiento2();
 
         if (_jumpAction.WasPressedThisFrame() && IsGrounded())
         {
@@ -56,26 +69,67 @@ public class PlayerController : MonoBehaviour
         Gravity();
     }
 
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
+
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+            
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _controller.Move(moveDirection * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Movimiento2()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        //Raycast
+        Ray ray = Camera.main.ScreenPointToRay(_lookInput);
+        RaycastHit hit;
+
+        //out lo que hace es sacar la información del impacto y almacenarla en la variable hit
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Vector3 playerForward = hit.point - transform.position;
+            Debug.Log(hit.transform.name);
+            playerForward.y = 0;
+            transform.forward = playerForward;
+        }
+        
+        if (direction != Vector3.zero)
+            {
+                _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime);
+            }
+    }
+
     void MovimientoCutre()
     {
-        _moveInput = _moveAction.ReadValue<Vector2>();
+        //_moveInput = _moveAction.ReadValue<Vector2>();
 
         Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
 
         if (direction != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
+
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
             _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime);
-        }        
+        }
     }
 
     void Jump()
     {
         _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+
         _controller.Move(_playerGravity * Time.deltaTime);
     }
 
@@ -106,6 +160,7 @@ public class PlayerController : MonoBehaviour
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
+
         Gizmos.DrawWireSphere(_sensor.position, _sensorRadius);
     }
 }
